f09625c9928a7addb3abd491d01f4992f011b45e
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2011-05-06 17:08:45 +0200

    Check for force_encoding before using it because of 1.8.7

--- lib/gimli/markup.rb
+++ lib/gimli/markup.rb
@@ -44,7 +44,8 @@ module Gimli
       data = extract_code(@data.dup)
       data = extract_tags(data)
       begin
-        data = GitHub::Markup.render(@filename, data.force_encoding('utf-8'))
+        data = data.force_encoding('utf-8') if data.respond_to? :force_encoding
+        data = GitHub::Markup.render(@filename, data)
         if data.nil?
           raise "There was an error converting #{@name} to HTML."
         end
@@ -54,7 +55,7 @@ module Gimli
       data = process_tags(data)
       data = process_code(data)
 
-      doc  = Nokogiri::HTML::DocumentFragment.parse(data.force_encoding('utf-8'))
+      doc  = Nokogiri::HTML::DocumentFragment.parse(data)
       yield doc if block_given?
       data = doc_to_html(doc)
 
