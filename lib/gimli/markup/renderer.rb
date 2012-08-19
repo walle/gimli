78677b05db48ae34864c24f05fc790427c0878a1
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2012-08-19 15:07:32 +0200

    Refactor markup renderer

--- lib/gimli/markup/renderer.rb
+++ lib/gimli/markup/renderer.rb
@@ -12,10 +12,12 @@ module Gimli
       # @param [Boolean] do_remove_yaml_front_matter Should we remove the front matter?
       # @return [Gimli::Markup]
       def initialize(file, do_remove_yaml_front_matter = false)
-        @filename = file.filename
-        @name = file.name
-        @data = file.data
+        @file = file
         @do_remove_yaml_front_matter = do_remove_yaml_front_matter
+
+        @data = file.data
+        @code = Code.new
+        @yaml_frontmatter_remover = YamlFrontmatterRemover.new
       end
 
       # Render the content with Gollum wiki syntax on top of the file's own
@@ -23,35 +25,40 @@ module Gimli
       #
       # @return [String] The formatted data
       def render
-        data = @data.dup
-        @code = Code.new
-        @yaml_frontmatter_remover = YamlFrontmatterRemover.new
-        data = @yaml_frontmatter_remover.process(data) if @do_remove_yaml_front_matter
-        data = @code.extract(data)
-        begin
-          data = data.force_encoding('utf-8') if data.respond_to? :force_encoding
-          data = GitHub::Markup.render(@filename, data)
-          if data.nil?
-            raise "There was an error converting #{@name} to HTML."
-          end
-        rescue Object => e
-          data = %{<p class="gimli-error">#{e.message}</p>}
-        end
-        data = @code.process(data)
+        prepare_data
+        render_data
+        post_process_data
 
-        doc  = Nokogiri::HTML::DocumentFragment.parse(data, 'UTF-8')
-        yield doc if block_given?
-        data = doc_to_html(doc)
-
-        data.gsub!(/<p><\/p>/, '')
-        data
+        return @data
       end
 
-      # Converts a nokogiri document fragment to html
-      # @param [Nokogiri::HTML::DocumentFragment] doc
-      # @return [String]
-      def doc_to_html(doc)
-        doc.to_xhtml(:save_with => Nokogiri::XML::Node::SaveOptions::AS_XHTML, :encoding => 'UTF-8')
+      private
+
+      # Prepare data for rendering
+      def prepare_data
+        @data = @yaml_frontmatter_remover.process(@data) if @do_remove_yaml_front_matter
+        @data = @code.extract(@data)
+      end
+
+      # Do the markup to html rendering
+      def render_data
+        begin
+          @data = @data.force_encoding('utf-8') if @data.respond_to? :force_encoding
+          @data = GitHub::Markup.render(@file.filename, @data)
+          if @data.nil?
+            raise "There was an error converting #{@file.name} to HTML."
+          end
+        rescue Object => e
+          @data = %{<p class="gimli-error">#{e.message}</p>}
+        end
+      end
+
+      # Do post processing on data
+      def post_process_data
+        @data = @code.process(@data)
+        doc  = Nokogiri::HTML::DocumentFragment.parse(@data, 'UTF-8')
+        @data = doc.to_xhtml(:save_with => Nokogiri::XML::Node::SaveOptions::AS_XHTML, :encoding => 'UTF-8')
+        @data.gsub!(/<p><\/p>/, '')
       end
     end
   end
