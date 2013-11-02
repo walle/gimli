25f2ea90c631a12afd16d4f9f534a9fc2b4f3f10
Takashi Kokubun <takashikkbn@gmail.com>
2013-11-02 19:22:05 +0900

    Add --cover option for creating cover with markdown file

--- lib/gimli/config.rb
+++ lib/gimli/config.rb
@@ -4,7 +4,7 @@ module Gimli
 
   # Class that keeps the config parameters
   class Config
-    attr_accessor :file, :recursive, :merge, :debug, :wkhtmltopdf_parameters, :remove_front_matter, :output_filename, :output_dir, :stylesheet
+    attr_accessor :file, :recursive, :merge, :debug, :wkhtmltopdf_parameters, :remove_front_matter, :output_filename, :output_dir, :stylesheet, :cover
 
     # Sets default values
     def initialize
